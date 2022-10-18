059305e583ed06fd7838a9d022a164b1371e2d84
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-19 08:05:31 +0900

    画像投稿機能を追加

---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+ActiveRecord::Schema.define(version: 2022_10_18_222853) do
+
+  enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
