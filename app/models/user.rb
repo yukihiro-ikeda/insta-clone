9d453f45eb6de16fe55663daaf057c32881ba36e
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-19 08:45:51 +0900

    ログインフォームを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
