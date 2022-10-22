903a1323726b158372c08f9adc8bf3858201ed4a
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-22 18:28:19 +0900

    ユーザ詳細画面にプロフィール画像を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }
   before_validation { email.downcase! }
