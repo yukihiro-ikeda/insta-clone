001108d0af626136e4ea5feca9370b17a083ac5d
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-24 14:13:34 +0900

    本番環境にレターオープナーを適用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'serene-earth-46031.herokuapp.com'}
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
